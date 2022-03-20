90ff5586e8990e710896de81e95f84249ffe31e3
Aossser <101010039+Aossser@users.noreply.github.com>
2022-03-20 16:24:31 +0700

    Add files via upload

---
+++ Ransomware2.0/Ransomware2.0/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Ransomware2._0
+{
+    partial class Ransomware2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ransomware2));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.codebox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tmr_hide = new System.Windows.Forms.Timer(this.components);
+            this.tmr_show = new System.Windows.Forms.Timer(this.components);
+            this.tmr_if = new System.Windows.Forms.Timer(this.components);
+            this.tmr_encrypt = new System.Windows.Forms.Timer(this.components);
+            this.tmr_clock = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox1.Image = global::Ransomware2._0.Properties.Resources.lock_padlock_symbol_for_protect_icon_icons_com_56926;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 393);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 133);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox2.Image = global::Ransomware2._0.Properties.Resources.lock_padlock_symbol_for_protect_icon_icons_com_56926;
+            this.pictureBox2.Location = new System.Drawing.Point(719, 393);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(143, 133);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.lbl1.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.White;
+            this.lbl1.Location = new System.Drawing.Point(68, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(721, 297);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = resources.GetString("lbl1.Text");
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(356, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "DECRYPT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // codebox
+            // 
+            this.codebox.BackColor = System.Drawing.Color.Red;
+            this.codebox.Location = new System.Drawing.Point(175, 342);
+            this.codebox.Name = "codebox";
+            this.codebox.Size = new System.Drawing.Size(524, 22);
+            this.codebox.TabIndex = 4;
+            this.codebox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(329, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "DECRYPT";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.No;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(204, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "REAMING TIME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(522, 470);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "00:00:00";
+            // 
+            // tmr_hide
+            // 
+            this.tmr_hide.Enabled = true;
+            this.tmr_hide.Interval = 10000;
+            // 
+            // tmr_show
+            // 
+            this.tmr_show.Enabled = true;
+            this.tmr_show.Interval = 1000;
+            // 
+            // tmr_if
+            // 
+            this.tmr_if.Enabled = true;
+            this.tmr_if.Interval = 300;
+            // 
+            // tmr_encrypt
+            // 
+            this.tmr_encrypt.Enabled = true;
+            this.tmr_encrypt.Interval = 1000;
+            // 
+            // tmr_clock
+            // 
+            this.tmr_clock.Enabled = true;
+            this.tmr_clock.Interval = 3600000;
+            // 
+            // Ransomware2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(874, 532);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.codebox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Ransomware2";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ransomware2.0";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Ransomware2_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox codebox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer tmr_hide;
+        private System.Windows.Forms.Timer tmr_show;
+        private System.Windows.Forms.Timer tmr_if;
+        private System.Windows.Forms.Timer tmr_encrypt;
+        private System.Windows.Forms.Timer tmr_clock;
+    }
+}
+
